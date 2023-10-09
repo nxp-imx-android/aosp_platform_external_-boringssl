26c05802166c7914e48cbf0d2a9be36217dd7d72
Ji Luo <ji.luo@nxp.com>
2023-10-09 16:09:32 +0800

    MA-21701 seed the boringssl with 'prediction resistance' random
    CRYPTO_sysrand() would generate random from hardware without the "PR"
    set on i.MX platforms. This commit uses "CRYPTO_sysrand_with_pr()" to
    generate 'prediction resistance' random and as the boringssl rng seed.
    
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Change-Id: I1315096d1cf6fa40378deaf7c34b7058be435c46

--- src/crypto/rand_extra/trusty.c
+++ src/crypto/rand_extra/trusty.c
@@ -31,8 +31,11 @@ void CRYPTO_sysrand(uint8_t *out, size_t requested) {
   }
 }
 
+void CRYPTO_sysrand_with_pr(uint8_t* out, size_t requested);
+
 void CRYPTO_sysrand_for_seed(uint8_t *out, size_t requested) {
-  CRYPTO_sysrand(out, requested);
+  /* generate 'prediction resistance' random */
+  CRYPTO_sysrand_with_pr(out, requested);
 }
 
 #endif  // OPENSSL_RAND_TRUSTY
